fa5bf8df6118ff77bf32aa09ea47c2364ff1e266
thaniav <61160291+thaniav@users.noreply.github.com>
2020-12-29 11:13:01 +0530

    Featured, categories and my stuff screens

--- lib/screens/Saved.dart
+++ lib/screens/Saved.dart
@@ -8,6 +8,34 @@ class Saved extends StatefulWidget {
 class _SavedState extends State<Saved> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Saved'),
+        backgroundColor: Colors.green,
+
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 50.0,
+
+            color: Color(0xFFF1EFF1),
+
+            child: Row(
+
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text('Recently Viewed', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      )
+    );
   }
 }
